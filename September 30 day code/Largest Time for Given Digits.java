3d1ca7941a2ae13e9b0f9dba21e695349f685e8e
Vrinda Gupta <50476758+Vrindagupta6828@users.noreply.github.com>
2020-09-01 18:37:03 +0530

    Update Largest Time for Given Digits.java

--- September 30 day code/Largest Time for Given Digits.java
+++ September 30 day code/Largest Time for Given Digits.java
@@ -21,43 +21,53 @@ Note:
 A.length == 4
 0 <= A[i] <= 9 */
 
-//Solution
-
-/*1. Keep a initial max value as 0000
-2. Generate all possible permuation that are possible.
-3. check function is used to check wheather the time lies between the range 00:00 and 23:59.
-4. if length of the generated string is 4, then compare with max value we had.
-5. return the max value string in required format.*/
+//Solution 2 ms
 
 class Solution {
-boolean[] vis;
-String max;
-boolean isPossible=false;
-public boolean check(String ans) {
-char[] r = ans.toCharArray();
-if(((r[0]-'0')<2 && (r[1]-'0')<=9 || (r[0]-'0')==2 && (r[1]-'0')<=3)&& (r[2]-'0')<=5 && (r[3]-'0')<=9)
-return true;
-return false;
-}
-public void dfs(int s, int[] A, String ans) {
-if(ans.length()==4 && check(ans)) {
-int num1 = Integer.parseInt(ans), num2 = Integer.parseInt(max);
-isPossible=true;
-if(num1 > num2) max = ans;
-}else {
-vis[s]=true;
-for(int i=0; i<4; i++)
-if(!vis[i]) dfs(i,A,ans+A[i]+"");
-vis[s]=false;
-}
-}
-public String largestTimeFromDigits(int[] A) {
-vis = new boolean[4];
-max="0000";
-for(int i=0; i<4; i++){
-dfs(i,A,A[i]+"");
-}
-if(max.equals("0000") && !isPossible) return "";
-return max.substring(0,2)+":"+max.substring(2,4);
-}
+    public String largestTimeFromDigits(int[] A) {
+    
+		int max = Integer.MIN_VALUE;
+
+		for (int i = 0; i < A.length; i++) {
+			for (int j = 0; j < A.length; j++) {
+
+				if (j != i) {
+					for (int k = 0; k < A.length; k++) {
+
+						if (k != i && k != j) {
+							int l = 6 - i - j - k;
+
+							int hrs = A[i] * 10 + A[j];
+							int mins = A[k] * 10 + A[l];
+
+							if (hrs < 24 && mins < 60) {
+
+								max = Math.max(hrs * 100 + mins, max);
+							}
+
+						}
+
+					}
+				}
+			}
+
+		}
+
+		StringBuilder sb = new StringBuilder(String.valueOf(max));
+	 if (max < 9) {
+			sb.insert(0, "000");
+
+		} else if (max < 99) {
+			sb.insert(0, "00");
+
+		} else
+
+		if (max < 999) {
+			sb.insert(0, "0");
+		}
+
+		sb.insert(2, ":");
+
+		return max == Integer.MIN_VALUE ? "" : sb.toString();
+    }
 }
