cf4c5f248160202d57fa48220b22ae177b63f703
Vrinda Gupta <50476758+Vrindagupta6828@users.noreply.github.com>
2020-09-05 13:48:59 +0530

    Create All Elements in Two Binary search Trees.java

---
+++ September 30 day code/All Elements in Two Binary search Trees.java
@@ -0,0 +1,71 @@
+/*Problem:
+Given two binary search trees root1 and root2.
+
+Return a list containing all the integers from both trees sorted in ascending order.
+
+ 
+
+Example 1:
+
+
+Input: root1 = [2,1,4], root2 = [1,0,3]
+Output: [0,1,1,2,3,4]
+Example 2:
+
+Input: root1 = [0,-10,10], root2 = [5,1,7,0,2]
+Output: [-10,0,0,1,2,5,7,10]
+Example 3:
+
+Input: root1 = [], root2 = [5,1,7,0,2]
+Output: [0,1,2,5,7]
+Example 4:
+
+Input: root1 = [0,-10,10], root2 = []
+Output: [-10,0,10]
+Example 5:
+
+
+Input: root1 = [1,null,8], root2 = [8,1]
+Output: [1,1,8,8]
+ 
+
+Constraints:
+
+Each tree has at most 5000 nodes.
+Each node's value is between [-10^5, 10^5]. */
+
+//Solution(91% 13 ms)
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
+        List<Integer> a=new ArrayList<>();
+        List<Integer> b=new ArrayList<>();
+        traverse(root1,a);
+        traverse(root2,b);
+        a.addAll(b);
+        Collections.sort(a);
+        return a;
+        
+    }
+    private void traverse(TreeNode root,List<Integer> k){
+        if(root==null)  return;
+        traverse(root.left,k);
+        k.add(root.val);
+        traverse(root.right,k);
+    }
+}
