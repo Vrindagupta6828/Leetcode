908fdcdd7d5e3b57d6f7dfa98ec84672d90b5343
Vrinda Gupta <50476758+Vrindagupta6828@users.noreply.github.com>
2020-09-22 12:38:13 +0530

    Create Plus One.java

---
+++ Top Interview Questions Easy Collection/Plus One.java
@@ -0,0 +1,44 @@
+/*Problem:
+Given a non-empty array of digits representing a non-negative integer, increment one to the integer.
+
+The digits are stored such that the most significant digit is at the head of the list, and each element in the array contains a single digit.
+
+You may assume the integer does not contain any leading zero, except the number 0 itself.
+
+ 
+
+Example 1:
+
+Input: digits = [1,2,3]
+Output: [1,2,4]
+Explanation: The array represents the integer 123.
+Example 2:
+
+Input: digits = [4,3,2,1]
+Output: [4,3,2,2]
+Explanation: The array represents the integer 4321.
+Example 3:
+
+Input: digits = [0]
+Output: [1]
+ 
+
+Constraints:
+
+1 <= digits.length <= 100
+0 <= digits[i] <= 9  */
+
+//My Solution(0 ms 100% better)
+class Solution {
+    public int[] plusOne(int[] digits) {
+        int i=digits.length-1;
+        while(i>=0 && digits[i]==9){
+            digits[i]=0;
+            i--;
+        }
+        if(i==-1){int[] ans=new int[digits.length+1];
+                 ans[0]=1;  return ans;}
+        digits[i]+=1;
+        return digits;
+    }
+}
