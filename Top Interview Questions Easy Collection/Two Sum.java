03dff51440ce9f718054b21933d09f0e8bf72a00
Vrinda Gupta <50476758+Vrindagupta6828@users.noreply.github.com>
2020-09-23 11:47:53 +0530

    Create Two Sum.java

---
+++ Top Interview Questions Easy Collection/Two Sum.java
@@ -0,0 +1,63 @@
+/*Problem:
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
+ 
+
+Constraints:
+
+2 <= nums.length <= 105
+-109 <= nums[i] <= 109
+-109 <= target <= 109
+Only one valid answer exists.  */
+
+//My Solution
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] ans=new int[2];
+        List<Integer> list = IntStream.of(nums).boxed().collect(Collectors.toList());
+        for(int i=0;i<list.size();i++){
+            int index=list.indexOf(target-nums[i]);
+            if(index!=i && index!=-1){
+                ans[0]=i;ans[1]=index;
+                break;
+            }
+        }
+        return ans;
+    }
+}
+
+//Solution(1 ms 99.98%)
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] ans=new int[2];
+        HashMap<Integer,Integer> map =new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            int diff=target-nums[i];
+            if(map.containsKey(diff)){
+                ans[0]=i;ans[1]=map.get(diff);
+                break;
+            }
+            map.put(nums[i],i);
+        }
+        return ans;
+    }
+}
