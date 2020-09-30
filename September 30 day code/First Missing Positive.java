dd32829e83589b202211078dba111fd455d3011d
Vrinda Gupta <50476758+Vrindagupta6828@users.noreply.github.com>
2020-09-30 14:42:26 +0530

    Create  First Missing Positive.java

---
+++ September 30 day code/First Missing Positive.java
@@ -0,0 +1,31 @@
+/* Problem:
+Given an unsorted integer array, find the smallest missing positive integer.
+
+Example 1:
+
+Input: [1,2,0]
+Output: 3
+Example 2:
+
+Input: [3,4,-1,1]
+Output: 2
+Example 3:
+
+Input: [7,8,9,11,12]
+Output: 1
+Follow up:
+
+Your algorithm should run in O(n) time and uses constant extra space.   */
+
+//My Solution
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        int ans=1;
+        for(int i=0;i<nums.length;i++){
+            map.put(nums[i],i);
+            while(map.containsKey(ans)) ans++;
+        }
+        return ans;
+    }
+}
