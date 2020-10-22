30684887263c3ec22ae1b54f3e925981afb940ac
Vrinda Gupta <50476758+Vrindagupta6828@users.noreply.github.com>
2020-10-22 13:03:52 +0530

    Create  Count Negative Numbers in a Sorted Matrix.java

---
+++ BinarySearch/Count Negative Numbers in a Sorted Matrix.java
@@ -0,0 +1,52 @@
+/* Question:
+Given a m * n matrix grid which is sorted in non-increasing order both row-wise and column-wise. 
+
+Return the number of negative numbers in grid.
+
+ 
+
+Example 1:
+
+Input: grid = [[4,3,2,-1],[3,2,1,-1],[1,1,-1,-2],[-1,-1,-2,-3]]
+Output: 8
+Explanation: There are 8 negatives number in the matrix.
+Example 2:
+
+Input: grid = [[3,2],[1,0]]
+Output: 0
+Example 3:
+
+Input: grid = [[1,-1],[-1,-1]]
+Output: 3
+Example 4:
+
+Input: grid = [[-1]]
+Output: 1
+ 
+
+Constraints:
+
+m == grid.length
+n == grid[i].length
+1 <= m, n <= 100
+-100 <= grid[i][j] <= 100   */
+
+//Solution 1(o(nlogm) time and o(1) space)
+class Solution {
+    public int countNegatives(int[][] grid) {
+        int count=0;
+        for(int i=0;i<grid.length;i++){
+            int start=0,end=grid[i].length-1;
+            while(start<=end){
+                int mid=(start+end)/2;
+                if(grid[i][mid]>=0)
+                    start=mid+1;
+                else{
+                    count+=1+end-mid;
+                    end=mid-1;
+                }
+            }
+        }
+        return count;
+    }
+}
