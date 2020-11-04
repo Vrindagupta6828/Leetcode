6cb6660d2d94cefae08749cd24a45ddae993b9fa
Vrinda Gupta <50476758+Vrindagupta6828@users.noreply.github.com>
2020-11-04 19:59:45 +0530

    Create Power of Four.java

---
+++ Bit Manipulation/Power of Four.java
@@ -0,0 +1,27 @@
+/* Question:
+Given an integer (signed 32 bits), write a function to check whether it is a power of 4.
+
+Example 1:
+
+Input: 16
+Output: true
+Example 2:
+
+Input: 5
+Output: false
+Follow up: Could you solve it without loops/recursion?  */
+
+//Solution 1 (O(n) in worst case)
+class Solution {
+    public boolean isPowerOfFour(int num) {
+    if ((num & num-1)==0){
+        int pos=-1;
+        while(num!=0){
+            pos++;
+            if((num&1)==1) break;
+            num>>=1;
+        }
+        if(pos%2==0) return true;}
+    return false;
+    }   
+}
