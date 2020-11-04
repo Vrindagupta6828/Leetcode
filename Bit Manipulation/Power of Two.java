d9b806895b44719a5c1186aa1f62134c73acc3fc
Vrinda Gupta <50476758+Vrindagupta6828@users.noreply.github.com>
2020-11-04 20:07:26 +0530

    Create Power of Two.java

---
+++ Bit Manipulation/Power of Two.java
@@ -0,0 +1,39 @@
+/* Question:
+Given an integer n, write a function to determine if it is a power of two.
+
+ 
+
+Example 1:
+
+Input: n = 1
+Output: true
+Explanation: 20 = 1
+Example 2:
+
+Input: n = 16
+Output: true
+Explanation: 24 = 16
+Example 3:
+
+Input: n = 3
+Output: false
+Example 4:
+
+Input: n = 4
+Output: true
+Example 5:
+
+Input: n = 5
+Output: false
+ 
+
+Constraints:
+
+-231 <= n <= 231 - 1  */
+
+//Solution(O(1))
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        return n>0 && (n&(n-1))==0;
+    }
+}
