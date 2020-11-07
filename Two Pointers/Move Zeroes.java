0a36316e4e8291381e47ac587781b947370ee327
Vrinda Gupta <50476758+Vrindagupta6828@users.noreply.github.com>
2020-11-07 12:49:47 +0530

    Update Move Zeroes.java

--- Two Pointers/Move Zeroes.java
+++ Two Pointers/Move Zeroes.java
@@ -15,13 +15,30 @@ class Solution {
     public void moveZeroes(int[] nums) {
         int i=0,j=0;
         while(i<nums.length && j<nums.length){
-            if(nums[j]==0) j++;
-            else{
+            if(nums[j]!=0){
                 int x=nums[i];
                 nums[i]=nums[j];
                 nums[j]=x;
-                i++;j++;
-            }
+                i++;}
+            j++;
         }
+    }
+}
+
+//Solution 2(O(n) time and O(1) space)
+void moveZeroes(vector<int>& nums) {
+    int lastNonZeroFoundAt = 0;
+    // If the current element is not 0, then we need to
+    // append it just in front of last non 0 element we found. 
+    for (int i = 0; i < nums.size(); i++) {
+        if (nums[i] != 0) {
+            nums[lastNonZeroFoundAt++] = nums[i];
+        }
+    }
+ 	// After we have finished processing new elements,
+ 	// all the non-zero elements are already at beginning of array.
+ 	// We just need to fill remaining array with 0's.
+    for (int i = lastNonZeroFoundAt; i < nums.size(); i++) {
+        nums[i] = 0;
     }
 }
