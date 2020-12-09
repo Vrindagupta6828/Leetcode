a9f1f977bc385206cd53d4a58fa46bed0382b0d7
Vrinda Gupta <50476758+Vrindagupta6828@users.noreply.github.com>
2020-12-09 12:33:34 +0530

    Update 706. Design HashMap.java

--- Hashing(HashMap)/706. Design HashMap.java
+++ Hashing(HashMap)/706. Design HashMap.java
@@ -157,3 +157,41 @@ class MyHashMap {
  * Your MyHashMap object will be instantiated and called as such: MyHashMap obj = new MyHashMap();
  * obj.put(key,value); int param_2 = obj.get(key); obj.remove(key);
  */
+
+//Solution 3
+class MyHashMap {
+    int[] arr;
+    boolean[] arr2;
+    /** Initialize your data structure here. */
+    public MyHashMap() {
+        arr=new int[1000000];
+        arr2=new boolean[1000000];
+    }
+    
+    /** value will always be non-negative. */
+    public void put(int key, int value) {
+        arr[key]=value;
+        arr2[key]=true;
+    }
+    
+    /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
+    public int get(int key) {
+        if(arr2[key]==true)
+            return arr[key];
+         return -1;
+    }
+    
+    /** Removes the mapping of the specified value key if this map contains a mapping for the key */
+    public void remove(int key) {
+        if(arr2[key]==true)
+        {arr2[key]=false;}
+    }
+}
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap obj = new MyHashMap();
+ * obj.put(key,value);
+ * int param_2 = obj.get(key);
+ * obj.remove(key);
+ */
