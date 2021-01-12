ff3acbec5e147ac89ee1b9c480da1060dc386854
Vrinda Gupta <50476758+Vrindagupta6828@users.noreply.github.com>
2021-01-12 13:03:26 +0530

    Update 257. Binary Tree Paths.java

--- Tree/257. Binary Tree Paths.java
+++ Tree/257. Binary Tree Paths.java
@@ -19,7 +19,7 @@ Explanation: All root-to-leaf paths are: 1->2->5, 1->3  */
 
 //Solution(O(n) time )
 /**
- * Definition for a binary tree node.
+  * Definition for a binary tree node.
  * public class TreeNode {
  *     int val;
  *     TreeNode left;
@@ -37,23 +37,24 @@ class Solution {
     List<String> ans;
     public List<String> binaryTreePaths(TreeNode root) {
         ans=new ArrayList<>();
-        traverse(root,"");
+        traverse(root,new StringBuilder());
         return ans;
     }
     
-    public void traverse(TreeNode root,String s){
+    public void traverse(TreeNode root,StringBuilder s){
         if(root==null)
             return;
         if(root.left==null && root.right==null)
-        {   s+=root.val;
-            ans.add(s);
+        {   s.append(root.val);
+            ans.add(s.toString());
             return;
         }
         else
         {
-            s+=root.val+"->";
-            traverse(root.left,s);
-            traverse(root.right,s);
+            s.append(root.val);
+            s.append("->");
+            traverse(root.left,new StringBuilder(s));
+            traverse(root.right,new StringBuilder(s));
         }
     }
 }
