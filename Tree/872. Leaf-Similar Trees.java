6462b5847b9ba6f1d6784f820c59f7653c952cf3
Vrinda Gupta <50476758+Vrindagupta6828@users.noreply.github.com>
2021-01-14 10:55:22 +0530

    Create 872. Leaf-Similar Trees.java

---
+++ Tree/872. Leaf-Similar Trees.java
@@ -0,0 +1,81 @@
+/* Question:
+Consider all the leaves of a binary tree, from left to right order, the values of those leaves form a leaf value sequence.
+
+
+
+For example, in the given tree above, the leaf value sequence is (6, 7, 4, 9, 8).
+
+Two binary trees are considered leaf-similar if their leaf value sequence is the same.
+
+Return true if and only if the two given trees with head nodes root1 and root2 are leaf-similar.
+
+ 
+
+Example 1:
+
+
+Input: root1 = [3,5,1,6,2,9,8,null,null,7,4], root2 = [3,5,1,6,7,4,2,null,null,null,null,null,null,9,8]
+Output: true
+Example 2:
+
+Input: root1 = [1], root2 = [1]
+Output: true
+Example 3:
+
+Input: root1 = [1], root2 = [2]
+Output: false
+Example 4:
+
+Input: root1 = [1,2], root2 = [2,2]
+Output: true
+Example 5:
+
+
+Input: root1 = [1,2,3], root2 = [1,3,2]
+Output: false
+ 
+
+Constraints:
+
+The number of nodes in each tree will be in the range [1, 200].
+Both of the given trees will have values in the range [0, 200].  */
+
+//Solution 1(O(n) time and o(k) space k-no. of leaf nodes)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+        ArrayList<Integer> leaf1=new ArrayList<>();
+        ArrayList<Integer> leaf2=new ArrayList<>();
+        traverse(root1,leaf1);
+        traverse(root2,leaf2);
+        return leaf1.equals(leaf2);
+    }
+    
+    private void traverse(TreeNode root,ArrayList<Integer> leaf){
+        if(root==null) return;
+        if(root.left==null && root.right==null)
+        {     leaf.add(root.val);
+              return;
+        }
+        else{
+            traverse(root.left,leaf);
+            traverse(root.right,leaf);
+        }
+    }
+    
+}
