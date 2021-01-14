feb49fe83bcd182c2a756ebf863024abce2cba5b
Vrinda Gupta <50476758+Vrindagupta6828@users.noreply.github.com>
2021-01-14 15:14:55 +0530

    Create 965. Univalued Binary Tree.java

---
+++ Tree/965. Univalued Binary Tree.java
@@ -0,0 +1,38 @@
+/* Question:
+A binary tree is univalued if every node in the tree has the same value.
+
+Return true if and only if the given tree is univalued.
+
+ 
+
+Example 1:
+
+
+Input: [1,1,1,1,1,null,1]
+Output: true
+Example 2:
+
+
+Input: [2,2,2,5,2]
+Output: false
+ 
+
+Note:
+
+The number of nodes in the given tree will be in the range [1, 100].
+Each node's value will be an integer in the range [0, 99].  */
+
+//Solution
+class Solution {
+    int v;
+    public boolean isUnivalTree(TreeNode root) {
+        if(root==null) return true;
+        v=root.val;
+        return traverse(root);
+    }
+    public boolean traverse(TreeNode root){
+        if(root==null) return true;
+        if(root.val!=v) return false;
+        return (traverse(root.left) && traverse(root.right));
+    }
+}
