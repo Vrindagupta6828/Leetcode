bcf26f371ecfd661e3ae87d28e7838d67dcd9621
Vrinda Gupta <50476758+Vrindagupta6828@users.noreply.github.com>
2021-01-14 10:59:15 +0530

    Create 226. Invert Binary Tree.java

---
+++ Tree/226. Invert Binary Tree.java
@@ -0,0 +1,79 @@
+/* Question:
+Invert a binary tree.
+
+Example:
+
+Input:
+
+     4
+   /   \
+  2     7
+ / \   / \
+1   3 6   9
+Output:
+
+     4
+   /   \
+  7     2
+ / \   / \
+9   6 3   1
+Trivia:
+This problem was inspired by this original tweet by Max Howell:
+
+Google: 90% of our engineers use the software you wrote (Homebrew), but you can’t invert a binary tree on a whiteboard so f*** off.  */
+
+//Solution 1(O(n) time and o(1) space)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+       if(root==null) return null;
+        root=new TreeNode(root.val,invertTree(root.right),invertTree(root.left));
+        return root;
+        
+    }
+}
+
+//their solution
+//1.recursive(o(n) time and o(h) space for recursion stack)
+
+public TreeNode invertTree(TreeNode root) {
+    if (root == null) {
+        return null;
+    }
+    TreeNode right = invertTree(root.right);
+    TreeNode left = invertTree(root.left);
+    root.left = right;
+    root.right = left;
+    return root;
+}
+
+//2.iterative(o(n) time and o(n) space)
+
+public TreeNode invertTree(TreeNode root) {
+    if (root == null) return null;
+    Queue<TreeNode> queue = new LinkedList<TreeNode>();
+    queue.add(root);
+    while (!queue.isEmpty()) {
+        TreeNode current = queue.poll();
+        TreeNode temp = current.left;
+        current.left = current.right;
+        current.right = temp;
+        if (current.left != null) queue.add(current.left);
+        if (current.right != null) queue.add(current.right);
+    }
+    return root;
+}
