198849ef2ce8969783efe76c14023cb57f16236e
Vrinda Gupta <50476758+Vrindagupta6828@users.noreply.github.com>
2021-01-19 12:13:27 +0530

    Create 53. Maximum Subarray.java

---
+++ Dynamic Programing/53. Maximum Subarray.java
@@ -0,0 +1,89 @@
+/* Question:
+Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+
+ 
+
+Example 1:
+
+Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+Output: 6
+Explanation: [4,-1,2,1] has the largest sum = 6.
+Example 2:
+
+Input: nums = [1]
+Output: 1
+Example 3:
+
+Input: nums = [0]
+Output: 0
+Example 4:
+
+Input: nums = [-1]
+Output: -1
+Example 5:
+
+Input: nums = [-100000]
+Output: -100000
+ 
+
+Constraints:
+
+1 <= nums.length <= 3 * 104
+-105 <= nums[i] <= 105
+ 
+
+Follow up: If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which is more subtle. */
+
+//Solution(O(n) time and o(1) space)
+class Solution {
+    public int maxSubArray(int[] nums) {
+       if(nums.length==1) return nums[0];
+        int[] dp=new int[nums.length];
+        dp[0]=nums[0];
+        int max=nums[0];
+        for(int i=1;i<nums.length;i++){
+            dp[i]=Math.max(dp[i-1]+nums[i],nums[i]);
+            max=Math.max(max,dp[i]);
+        }
+       return max;
+    }
+}
+
+//Solution 2(divide an dconquer(o(nlogn) time))
+class Solution {
+  public int crossSum(int[] nums, int left, int right, int p) {
+    if (left == right) return nums[left];
+
+    int leftSubsum = Integer.MIN_VALUE;
+    int currSum = 0;
+    for(int i = p; i > left - 1; --i) {
+      currSum += nums[i];
+      leftSubsum = Math.max(leftSubsum, currSum);
+    }
+
+    int rightSubsum = Integer.MIN_VALUE;
+    currSum = 0;
+    for(int i = p + 1; i < right + 1; ++i) {
+      currSum += nums[i];
+      rightSubsum = Math.max(rightSubsum, currSum);
+    }
+
+    return leftSubsum + rightSubsum;
+  }
+
+  public int helper(int[] nums, int left, int right) {
+    if (left == right) return nums[left];
+
+    int p = (left + right) / 2;
+
+    int leftSum = helper(nums, left, p);
+    int rightSum = helper(nums, p + 1, right);
+    int crossSum = crossSum(nums, left, right, p);
+
+    return Math.max(Math.max(leftSum, rightSum), crossSum);
+  }
+
+  public int maxSubArray(int[] nums) {
+    return helper(nums, 0, nums.length - 1);
+  }
+}
