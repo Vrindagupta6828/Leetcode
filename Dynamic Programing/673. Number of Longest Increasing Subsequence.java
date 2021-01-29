fe719eb41d049bec7d1e26d557d1eb53b3fef5b6
Vrinda Gupta <50476758+Vrindagupta6828@users.noreply.github.com>
2021-01-29 14:12:55 +0530

    Create 673. Number of Longest Increasing Subsequence.java

---
+++ Dynamic Programing/673. Number of Longest Increasing Subsequence.java
@@ -0,0 +1,57 @@
+/* Question:
+Given an integer array nums, return the number of longest increasing subsequences.
+
+Notice that the sequence has to be strictly increasing.
+
+ 
+
+Example 1:
+
+Input: nums = [1,3,5,4,7]
+Output: 2
+Explanation: The two longest increasing subsequences are [1, 3, 4, 7] and [1, 3, 5, 7].
+Example 2:
+
+Input: nums = [2,2,2,2,2]
+Output: 5
+Explanation: The length of longest continuous increasing subsequence is 1, and there are 5 subsequences' length is 1, so output 5.
+
+ 
+
+Constraints:
+
+1 <= nums.length <= 2000
+-106 <= nums[i] <= 106  */
+
+//Solution 1(o(n^2) time and o(n) space)
+class Solution {
+    public int findNumberOfLIS(int[] nums) {
+        int N = nums.length;
+        if (N <= 1) return N;
+        int[] lengths = new int[N]; //lengths[i] = length of longest ending in nums[i]
+        int[] counts = new int[N]; //count[i] = number of longest ending in nums[i]
+        Arrays.fill(counts, 1);
+
+        for (int j = 0; j < N; ++j) {
+            for (int i = 0; i < j; ++i) if (nums[i] < nums[j]) {
+                if (lengths[i] >= lengths[j]) {
+                    lengths[j] = lengths[i] + 1;
+                    counts[j] = counts[i];
+                } else if (lengths[i] + 1 == lengths[j]) {
+                    counts[j] += counts[i];
+                }
+            }
+        }
+
+        int longest = 0, ans = 0;
+        for (int length: lengths) {
+            longest = Math.max(longest, length);
+        }
+        for (int i = 0; i < N; ++i) {
+            if (lengths[i] == longest) {
+                ans += counts[i];
+            }
+        }
+        return ans;
+    }
+}
