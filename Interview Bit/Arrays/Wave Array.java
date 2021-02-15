858b3d1c2f2d5aad16f485c005678e81c1d28c94
Vrinda Gupta <50476758+Vrindagupta6828@users.noreply.github.com>
2021-02-15 11:13:07 +0530

    Create Wave Array.java

---
+++ Interview Bit/Arrays/Wave Array.java
@@ -0,0 +1,28 @@
+/* Question:
+Given an array of integers, sort the array into a wave like array and return it,
+In other words, arrange the elements into a sequence such that a1 >= a2 <= a3 >= a4 <= a5.....
+
+Example
+
+Given [1, 2, 3, 4]
+
+One possible answer : [2, 1, 4, 3]
+Another possible answer : [4, 1, 3, 2]
+ NOTE : If there are multiple answers possible, return the one thats lexicographically smallest.
+So, in example case, you will return [2, 1, 4, 3]   */
+
+//Solution o(nlogn)
+public class Solution {
+    public ArrayList<Integer> wave(ArrayList<Integer> A) {
+        int n=A.size();
+        Collections.sort(A);
+        
+        for(int i=0;i<n-1;i+=2){
+            int temp=A.get(i+1);
+            A.set(i+1,A.get(i));
+            A.set(i,temp);
+        }
+        
+        return A;
+    }
+}
