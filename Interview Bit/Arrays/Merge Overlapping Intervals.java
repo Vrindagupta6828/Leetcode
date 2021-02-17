fee24a7c70a7082ce96cef4c9eb1d40efe9cd8b0
Vrinda Gupta <50476758+Vrindagupta6828@users.noreply.github.com>
2021-02-17 12:23:18 +0530

    Update Merge Overlapping Intervals.java

--- Interview Bit/Arrays/Merge Overlapping Intervals.java
+++ Interview Bit/Arrays/Merge Overlapping Intervals.java
@@ -49,3 +49,38 @@ public class Solution {
           return ans;
     }
 }
+
+//Solution 2
+/**
+ * Definition for an interval.
+ * public class Interval {
+ *     int start;
+ *     int end;
+ *     Interval() { start = 0; end = 0; }
+ *     Interval(int s, int e) { start = s; end = e; }
+ * }
+ */
+public class Solution {
+    public ArrayList<Interval> merge(ArrayList<Interval> intervals) {
+          int n=intervals.size();
+          ArrayList<Interval> ans=new ArrayList<>();
+          Collections.sort(intervals, (a, b) -> Integer.compare(a.start, b.start));
+          
+          Interval current=intervals.get(0);
+          
+         
+          for(int i=1;i<n;i++){
+              Interval check=intervals.get(i);
+              if(current.end<check.start)
+                {   ans.add(current);
+                    current=check;
+                }
+              else
+                current.end=Math.max(current.end,check.end);
+          }  
+           
+          ans.add(current);      
+          return ans;
+    }
+}
+
