15c220134cdc27f258655c22f926bcea13ed4be8
Vrinda Gupta <50476758+Vrindagupta6828@users.noreply.github.com>
2021-02-18 11:52:31 +0530

    Create Pascal Triangle.java

---
+++ Interview Bit/Arrays/Pascal Triangle.java
@@ -0,0 +1,35 @@
+/* Question:
+Given numRows, generate the first numRows of Pascal’s triangle.
+
+Pascal’s triangle : To generate A[C] in row R, sum up A’[C] and A’[C-1] from previous row R - 1.
+
+Example:
+
+Given numRows = 5,
+
+Return
+
+[
+     [1],
+     [1,1],
+     [1,2,1],
+     [1,3,3,1],
+     [1,4,6,4,1]
+]    */
+
+//Solution (o(n^2) time and o(1) space)
+public class Solution {
+    public ArrayList<ArrayList<Integer>> solve(int A) {
+        ArrayList<ArrayList<Integer>> ans=new ArrayList<>();
+        for(int i=1;i<=A;i++){
+            int C=1;
+            ArrayList<Integer> temp=new ArrayList<>();
+            for(int j=1;j<=i;j++){
+                temp.add(C);
+                C=C*(i-j)/j;
+            }
+            ans.add(temp);
+        }
+        return ans;
+    }
+}
