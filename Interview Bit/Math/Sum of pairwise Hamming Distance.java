aecef23b6d394821b73dcc7e827c9673bb5b7701
Vrinda Gupta <50476758+Vrindagupta6828@users.noreply.github.com>
2021-03-03 13:58:47 +0530

    Create Sum of pairwise Hamming Distance.java

---
+++ Interview Bit/Math/Sum of pairwise Hamming Distance.java
@@ -0,0 +1,69 @@
+/* Question:
+Problem Description
+
+Hamming distance between two non-negative integers is defined as the number of positions at which the corresponding bits are different.
+
+Given an array A of N non-negative integers, find the sum of hamming distances of all pairs of integers in the array. Return the answer modulo 1000000007.
+
+
+
+Problem Constraints
+1 <= |A| <= 200000
+
+1 <= A[i] <= 109
+
+
+
+Input Format
+First and only argument is array A.
+
+
+
+Output Format
+Return one integer, the answer to the problem.
+
+
+
+Example Input
+Input 1:
+
+ A = [1]
+Input 2:
+
+ A = [2, 4, 6]
+
+
+Example Output
+Output 1:
+
+ 0
+Output 2:
+
+ 8
+
+
+Example Explanation
+Explanation 1:
+
+ No pairs are formed.
+Explanation 2:
+
+ We return, f(2, 2) + f(2, 4) + f(2, 6) + f(4, 2) + f(4, 4) + f(4, 6) + f(6, 2) + f(6, 4) + f(6, 6) = 8  */
+
+//Solution
+public class Solution {
+    // DO NOT MODIFY THE LIST. IT IS READ ONLY
+    public int hammingDistance(final List<Integer> A) {
+        int n=A.size(),ans=0;
+        for(int i=0;i<31;i++){
+            int one=0,zero=0;
+            for(int j=0;j<n;j++){
+                if((A.get(j)&(1<<i))!=0) one++;
+                else zero++;
+            }
+                ans+=(2L*one*zero)%1000000007;
+                ans=ans%1000000007;
+            }
+        return ans;
+    }
+}
