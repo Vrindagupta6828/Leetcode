cb0c98b69e25f936103667d536d7f92148a1a552
Vrinda Gupta <50476758+Vrindagupta6828@users.noreply.github.com>
2021-03-13 11:36:03 +0530

    Create Wave Array.java

---
+++ Goldman/Wave Array.java
@@ -0,0 +1,43 @@
+/* Question:
+Given a sorted array arr[] of distinct integers. Sort the array into a wave-like array and return it. In other words, arrange the elements into a sequence such that a1 >= a2 <= a3 >= a4 <= a5..... (considering the increasing lexicographical order).
+
+Example 1:
+
+Input:
+n = 5
+arr[] = {1,2,3,4,5}
+Output: 2 1 4 3 5
+Explanation: Array elements after 
+sorting it in wave form are 
+2 1 4 3 5.
+ 
+
+Example 2:
+
+Input:
+n = 6
+arr[] = {2,4,7,8,9,10}
+Output: 4 2 8 7 10 9
+Explanation: Array elements after 
+sorting it in wave form are 
+4 2 8 7 10 9.
+Your Task:
+The task is to complete the function convertToWave() which converts the given array to wave array.
+
+Expected Time Complexity: O(n).
+Expected Auxiliary Space: O(1).
+
+Constraints:
+1 ≤ n ≤ 106
+0 ≤ A[i] ≤107   */
+
+//Solution
+public static void convertToWave(int arr[], int n){
+        
+        // Your code here
+        for(int i=0;i<=n-2;i+=2){
+            int temp=arr[i];
+            arr[i]=arr[i+1];
+            arr[i+1]=temp;
+        }
+    }
